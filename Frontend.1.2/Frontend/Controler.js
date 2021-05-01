49de8055e328ba382e20e20cd39bb6697a36e230
Nil_Piñana <61458810+Nil729@users.noreply.github.com>
2021-05-01 10:48:55 +0200

    Add files via upload

---
+++ Frontend.1.2/Frontend/Controler.js
@@ -0,0 +1,19 @@
+// De moment no posar aixó:
+
+function postRutina(){
+    
+    console.log("hola bon dia")
+    var classeMyRutina = "ContenedorMy_Rutina";
+
+    const ContenedorMy_Rutina = document.querySelector(".ContenedorDe_Rutina");
+    ContenedorMy_Rutina.innerHTML = "<section class= ",classeMyRutina,"></section>";
+    ContenedorMy_Rutina.outerHTML;     // "<div class="data"><h1>Tema 1</h1></div>"  // <div></div>
+};
+/*function supRutina{
+
+}
+
+function viwRutina{
+
+}*/
+
